8194411eea1a634d800f5a8c80c29223b266a07d
Mehul Arora <mehul_arora@live.com>
2024-10-09 08:27:14 -0400

    ..

--- src/stream.rs
+++ src/stream.rs
@@ -1,6 +1,7 @@
 use streamstore::{
     client::StreamClient,
     service_error::{AppendSessionError, GetNextSeqNumError, ReadSessionError, ServiceError},
+    streams::AppendRecordStream,
     types::{AppendOutput, ReadSessionRequest, ReadSessionResponse},
     Streaming,
 };
@@ -9,85 +10,38 @@ use tokio::io::AsyncBufRead;
 use pin_project_lite::pin_project;
 use std::pin::Pin;
 use std::task::{Context, Poll};
-use streamstore::types::{AppendInput, AppendRecord};
+use streamstore::types::AppendRecord;
 use tokio::io::Lines;
 use tokio_stream::Stream;
 
 pin_project! {
     #[derive(Debug)]
-    /// A stream of records produced by polling for a new line from the underlying [`Lines`] stream.
-    /// Chunk up either 1MiB of records or 1000 records, whichever comes first.
     pub struct RecordStream<R> {
         #[pin]
         inner: Lines<R>,
-        peeked_record: Option<String>,
     }
 }
 
 impl<R> RecordStream<R> {
     pub fn new(lines: Lines<R>) -> Self {
-        Self {
-            inner: lines,
-            peeked_record: None,
-        }
+        Self { inner: lines }
     }
 }
 
 impl<R: AsyncBufRead> Stream for RecordStream<R> {
-    type Item = AppendInput;
+    type Item = AppendRecord;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let mut this = self.project();
-        let mut num_records = 0;
-        let mut batch_size = 0;
-        let mut append_input = AppendInput::new(Vec::with_capacity(1000));
-
-        if let Some(record) = &this.peeked_record.take() {
-            append_input.records.push(AppendRecord::new(record.clone()));
-            num_records += 1;
-            batch_size += record.len() + 8;
+        match this.inner.as_mut().poll_next_line(cx) {
+            Poll::Ready(Ok(Some(line))) => Poll::Ready(Some(AppendRecord::new(line))),
+            Poll::Ready(Ok(None)) => Poll::Ready(None),
+            Poll::Ready(Err(e)) => {
+                eprintln!("Error reading line: {}", e);
+                Poll::Ready(None)
+            }
+            Poll::Pending => Poll::Pending,
         }
-
-        while num_records < 1000 {
-            let line = match this.inner.as_mut().poll_next_line(cx) {
-                Poll::Ready(Ok(Some(line))) => {
-                    // + 8 for constant overhead to deal with encoding bits
-                    if batch_size + line.len() + 8 > 1024 * 1024 {
-                        *this.peeked_record = Some(line.clone());
-                        break;
-                    }
-                    batch_size += line.len() + 8;
-                    line
-                }
-                Poll::Ready(Err(e)) => {
-                    eprintln!("Error reading line: {}", e);
-                    return Poll::Ready(None);
-                }
-                Poll::Ready(Ok(None)) => {
-                    // EOF
-                    if num_records == 0 {
-                        return Poll::Ready(None);
-                    } else {
-                        break;
-                    }
-                }
-                Poll::Pending => {
-                    if num_records == 0 {
-                        return Poll::Pending;
-                    } else {
-                        break;
-                    }
-                }
-            };
-            num_records += 1;
-            append_input.records.push(AppendRecord::new(line));
-        }
-
-        eprintln!(
-            "[SENT]: read [{} records], assumed batch size [{} bytes]",
-            num_records, batch_size
-        );
-        Poll::Ready(Some(append_input))
     }
 }
 
@@ -118,9 +72,12 @@ impl StreamService {
 
     pub async fn append_session(
         &self,
-        stream: RecordStream<Box<dyn AsyncBufRead + Send + Unpin>>,
+        append_input_stream: RecordStream<Box<dyn AsyncBufRead + Send + Unpin>>,
     ) -> Result<Streaming<AppendOutput, AppendSessionError>, StreamServiceError> {
-        Ok(self.client.append_session(stream).await?)
+        let append_record_stream = AppendRecordStream::new(append_input_stream, Default::default())
+            .expect("stream init can't fail");
+
+        Ok(self.client.append_session(append_record_stream).await?)
     }
 
     pub async fn read_session(
