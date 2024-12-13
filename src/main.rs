1db38a3128aa6cd64873131b126d25ebb2752f00
Mehul Arora <65443164+infiniteregrets@users.noreply.github.com>
2024-12-12 21:58:18 -0500

    fix: disable noisy description in help (#79)

--- src/main.rs
+++ src/main.rs
@@ -61,7 +61,7 @@ const GENERAL_USAGE: &str = color_print::cstr!(
 );
 
 #[derive(Parser, Debug)]
-#[command(version, about, override_usage = GENERAL_USAGE, styles = STYLES)]
+#[command(version, override_usage = GENERAL_USAGE, styles = STYLES)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
