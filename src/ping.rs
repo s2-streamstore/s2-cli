s2-streamstore/s2-cli

commit a8d11caf26308b0235b7c337459ce87c77a141de

feat: support reading from timestamp or tail-offset (#137)
... and misc updates

src/ping.rs
@@ -1,9 +1,11 @@
 use std::time::Duration;
 
-use rand::{Rng, distributions::Uniform};
+use rand::Rng;
 use s2::{
     batching::AppendRecordsBatchingOpts,
-    types::{AppendOutput, AppendRecord, ReadOutput, SequencedRecord, SequencedRecordBatch},
+    types::{
+        AppendAck, AppendRecord, ReadOutput, ReadStart, SequencedRecord, SequencedRecordBatch,
+    },
 };
 use tokio::{join, select, signal, sync::mpsc, task::JoinHandle, time::Instant};
 use tokio_stream::StreamExt;
@@ -31,7 +33,9 @@ impl Pinger {
     pub async fn init(stream_client: &StreamService) -> Result<Self, S2CliError> {
         let tail = stream_client.check_tail().await?;
 
-        let mut read_stream = stream_client.read_session(tail, None, None).await?;
+        let mut read_stream = stream_client
+            .read_session(ReadStart::SeqNum(tail.seq_num), None, None)
+            .await?;
 
         let (records_tx, records_rx) = mpsc::unbounded_channel();
         let mut append_stream = stream_client
@@ -39,7 +43,7 @@ impl Pinger {
                 tokio_stream::wrappers::UnboundedReceiverStream::new(records_rx),
                 AppendRecordsBatchingOpts::new()
                     .with_max_batch_records(1)
-                    .with_match_seq_num(Some(tail)),
+                    .with_match_seq_num(Some(tail.seq_num)),
             )
             .await?;
 
@@ -49,7 +53,7 @@ impl Pinger {
             .expect("stream channel open");
 
         match append_stream.next().await.expect("warmup batch ack") {
-            Ok(AppendOutput { start_seq_num, .. }) if start_seq_num == tail => (),
+            Ok(AppendAck { start, .. }) if start == tail => (),
             Ok(_) => return Err(S2CliError::PingStreamMutated),
             Err(e) => return Err(ServiceError::new(ServiceErrorContext::AppendSession, e).into()),
         };
@@ -103,13 +107,9 @@ impl Pinger {
         let appends_handle = tokio::spawn(async move {
             while let Some(next) = append_stream.next().await {
                 match next {
-                    Ok(AppendOutput {
-                        start_seq_num,
-                        end_seq_num,
-                        ..
-                    }) => {
+                    Ok(AppendAck { start, end, .. }) => {
                         let append = Instant::now();
-                        let records = end_seq_num - start_seq_num;
+                        let records = end.seq_num - start.seq_num;
                         if records != 1 {
                             appends_tx
                                 .send(Err(S2CliError::PingStreamMutated))
@@ -140,8 +140,10 @@ impl Pinger {
     }
 
     pub async fn ping(&mut self, bytes: u64) -> Result<Option<PingResult>, S2CliError> {
-        let body = rand::thread_rng()
-            .sample_iter(&Uniform::new_inclusive(0, u8::MAX))
+        let body = rand::rng()
+            .sample_iter(
+                rand::distr::Uniform::new_inclusive(0, u8::MAX).expect("valid distribution"),
+            )
             .take(bytes as usize)
             .collect::<Vec<_>>();
 

Cargo.toml
@@ -14,31 +14,31 @@ path = "src/main.rs"
 
 [dependencies]
 async-stream = "0.3.6"
-base64ct = { version = "1.6.0", features = ["alloc"] }
-bytes = "1.9.0"
-clap = { version = "4.5.27", features = ["derive"] }
+base64ct = { version = "1.7.3", features = ["alloc"] }
+bytes = "1.10.1"
+clap = { version = "4.5.37", features = ["derive"] }
 color-print = "0.3.7"
 colored = "3.0.0"
-config = "0.15.6"
+config = "0.15.11"
 dirs = "6.0.0"
 futures = "0.3.31"
-humantime = "2.1.0"
-indicatif = "0.17.9"
-json_to_table = "0.10.0"
-miette = { version = "7.4.0", features = ["fancy"] }
-rand = "0.8.5"
-serde = { version = "1.0.217", features = ["derive"] }
-serde_json = "1.0.137"
-streamstore = "0.11.0"
-thiserror = "2.0.11"
-tokio = { version = "1.43.0", features = ["full"] }
+humantime = "2.2.0"
+indicatif = "0.17.11"
+json_to_table = "0.11.0"
+miette = { version = "7.6.0", features = ["fancy"] }
+rand = "0.9.1"
+serde = { version = "1.0.219", features = ["derive"] }
+serde_json = "1.0.140"
+streamstore = "0.13.0"
+thiserror = "2.0.12"
+tokio = { version = "1.44.2", features = ["full"] }
 tokio-stream = { version = "0.1.17", features = ["io-util"] }
-toml = "0.8.19"
+toml = "0.8.22"
 tracing = "0.1.41"
 tracing-subscriber = { version = "0.3.19", features = ["env-filter"] }
 
 [dev-dependencies]
-rstest = "0.18.2"
+rstest = "0.25.0"
 
 [profile.release]
 lto = true

src/account.rs
@@ -144,7 +144,7 @@ impl AccountService {
         auto_prefix_streams: bool,
         basins: Option<ResourceSet>,
         streams: Option<ResourceSet>,
-        tokens: Option<ResourceSet>,
+        access_tokens: Option<ResourceSet>,
         op_groups: Option<PermittedOperationGroups>,
         ops: Vec<Operation>,
     ) -> Result<String, ServiceError> {
@@ -155,8 +155,8 @@ impl AccountService {
         if let Some(streams) = streams {
             access_token_scope = access_token_scope.with_streams(streams);
         }
-        if let Some(tokens) = tokens {
-            access_token_scope = access_token_scope.with_tokens(tokens);
+        if let Some(access_tokens) = access_tokens {
+            access_token_scope = access_token_scope.with_tokens(access_tokens);
         }
         if let Some(op_groups) = op_groups {
             access_token_scope = access_token_scope.with_op_groups(op_groups);
@@ -199,14 +199,14 @@ impl AccountService {
                     .await;
 
                 match resp.as_ref() {
-                    Ok(ListAccessTokensResponse { tokens, has_more }) if *has_more && !no_auto_paginate => {
-                            start_after = tokens
+                    Ok(ListAccessTokensResponse { access_tokens, has_more }) if *has_more && !no_auto_paginate => {
+                            start_after = access_tokens
                                 .last()
                                 .map(|s| s.id.clone().into())
                                 .ok_or(ServiceError::new(ServiceErrorContext::ListAccessTokens, ServiceStatus::default()))?;
                             if let Some(l) = limit {
-                                if l > tokens.len() {
-                                    limit = Some(l - tokens.len());
+                                if l > access_tokens.len() {
+                                    limit = Some(l - access_tokens.len());
                                 } else {
                                     return yield resp;
                                 }

src/main.rs
@@ -5,7 +5,7 @@ use std::{
     path::PathBuf,
     pin::Pin,
     str::FromStr,
-    time::{Duration, UNIX_EPOCH},
+    time::{Duration, SystemTime, UNIX_EPOCH},
 };
 
 use account::AccountService;
@@ -24,7 +24,7 @@ use s2::{
     client::{BasinClient, Client, ClientConfig, S2Endpoints, StreamClient},
     types::{
         AccessTokenId, AppendRecord, AppendRecordBatch, BasinInfo, Command, CommandRecord,
-        ConvertError, FencingToken, MeteredBytes as _, ReadOutput, StreamInfo,
+        ConvertError, FencingToken, MeteredBytes as _, ReadOutput, ReadStart, StreamInfo,
     },
 };
 use stream::{RecordStream, StreamService};
@@ -197,11 +197,11 @@ enum Commands {
         /// Token IDs allowed.
         /// Matches exact value if it starts with `=`, otherwise treats it as a prefix.
         #[arg(long)]
-        tokens: Option<ResourceSet<1, 50>>,
+        access_tokens: Option<ResourceSet<1, 96>>,
 
         /// Access permissions at the group level.
         /// The format is: "account=rw,basin=r,stream=w"
-        /// where 'r' indicates read permission and 'w' indicates write permission.        
+        /// where 'r' indicates read permission and 'w' indicates write permission.
         #[arg(long)]
         op_groups: Option<PermittedOperationGroups>,
 
@@ -380,25 +380,38 @@ enum Commands {
         uri: S2BasinAndStreamUriArgs,
 
         /// Starting sequence number (inclusive).
-        #[arg(short = 's', long, default_value_t = 0)]
-        start_seq_num: u64,
+        #[arg(short = 's', long, group = "start")]
+        seq_num: Option<u64>,
+
+        /// Starting timestamp (inclusive).
+        #[arg(long, group = "start")]
+        timestamp: Option<u64>,
+
+        /// Starting timestamp as a human-friendly delta from current time e.g. "1h",
+        /// which will be converted to milliseconds since Unix epoch.
+        #[arg(long, group = "start")]
+        ago: Option<humantime::Duration>,
+
+        /// Start from N records before the tail of the stream.
+        #[arg(long, group = "start")]
+        tail_offset: Option<u64>,
 
         /// Output format.
         #[arg(long, value_enum, default_value_t)]
         format: Format,
 
+        /// Limit the number of records returned.
+        #[arg(short = 'n', long)]
+        count: Option<u64>,
+
+        /// Limit the number of bytes returned.
+        #[arg(short = 'b', long)]
+        bytes: Option<u64>,
+
         /// Output records to a file or stdout.
         /// Use "-" to write to stdout.
         #[arg(short = 'o', long, value_parser = parse_records_output_source, default_value = "-")]
         output: RecordsOut,
-
-        /// Limit the number of records returned.
-        #[arg(short = 'n', long)]
-        limit_count: Option<u64>,
-
-        /// Limit the number of bytes returned.
-        #[arg(short = 'b', long)]
-        limit_bytes: Option<u64>,
     },
 
     /// Ping the stream to get append acknowledgement and end-to-end latencies.
@@ -712,7 +725,7 @@ async fn run() -> Result<(), S2CliError> {
         tokio::pin!(tokens);
 
         while let Some(token) = tokens.next().await {
-            for token_info in token?.tokens {
+            for token_info in token?.access_tokens {
                 let exp_date = token_info
                     .expires_at
                     .map(|exp| {
@@ -931,8 +944,8 @@ async fn run() -> Result<(), S2CliError> {
             let cfg = config::load_config(&config_path)?;
             let client_config = client_config(cfg.access_token)?;
             let stream_client = StreamClient::new(client_config, basin, stream);
-            let next_seq_num = StreamService::new(stream_client).check_tail().await?;
-            println!("{}", next_seq_num);
+            let tail = StreamService::new(stream_client).check_tail().await?;
+            println!("{}\t{}", tail.seq_num, tail.timestamp);
         }
         Commands::Trim {
             uri,
@@ -954,8 +967,8 @@ async fn run() -> Result<(), S2CliError> {
             eprintln!(
                 "{}",
                 format!(
-                    "✓ Trim request for trim point {} appended at seq_num: {}",
-                    trim_point, out.start_seq_num,
+                    "✓ Trim request for trim point {trim_point} appended at: {:?}",
+                    out.start
                 )
                 .green()
                 .bold()
@@ -980,7 +993,7 @@ async fn run() -> Result<(), S2CliError> {
                 .await?;
             eprintln!(
                 "{}",
-                format!("✓ Fencing token appended at seq_num: {}", out.start_seq_num)
+                format!("✓ Fencing token appended at seq_num: {:?}", out.start)
                     .green()
                     .bold()
             );
@@ -1030,10 +1043,11 @@ async fn run() -> Result<(), S2CliError> {
                                         eprintln!(
                                             "{}",
                                             format!(
-                                                "✓ [APPENDED] start: {}, end: {}, next: {}",
-                                                append_result.start_seq_num,
-                                                append_result.end_seq_num,
-                                                append_result.next_seq_num
+                                                "✓ [APPENDED] {}..{} // tail: {} @ {}",
+                                                append_result.start.seq_num,
+                                                append_result.end.seq_num,
+                                                append_result.tail.seq_num,
+                                                append_result.tail.timestamp
                                             )
                                             .green()
                                             .bold()
@@ -1058,18 +1072,39 @@ async fn run() -> Result<(), S2CliError> {
         }
         Commands::Read {
             uri,
-            start_seq_num,
+            seq_num,
+            timestamp,
+            tail_offset,
+            ago,
             output,
-            limit_count,
-            limit_bytes,
+            count,
+            bytes,
             format,
         } => {
             let S2BasinAndStreamUri { basin, stream } = uri.uri;
             let cfg = config::load_config(&config_path)?;
             let client_config = client_config(cfg.access_token)?;
             let stream_client = StreamClient::new(client_config, basin, stream);
+
+            let read_start = match (seq_num, timestamp, tail_offset, ago) {
+                (Some(seq_num), None, None, None) => ReadStart::SeqNum(seq_num),
+                (None, Some(timestamp), None, None) => ReadStart::Timestamp(timestamp),
+                (None, None, Some(offset), None) => ReadStart::TailOffset(offset),
+                (None, None, None, Some(ago)) => {
+                    let timestamp = SystemTime::now()
+                        .duration_since(SystemTime::UNIX_EPOCH)
+                        .unwrap()
+                        .as_millis()
+                        .saturating_sub(ago.as_millis()) as u64;
+                    ReadStart::Timestamp(timestamp)
+                }
+                // Default to beginning of stream
+                (None, None, None, None) => s2::types::ReadStart::SeqNum(0),
+                _ => unreachable!("clap ensures only one start option is provided"),
+            };
+
             let mut read_output_stream = StreamService::new(stream_client)
-                .read_session(start_seq_num, limit_count, limit_bytes)
+                .read_session(read_start, count, bytes)
                 .await?;
             let mut writer = output.into_writer().await.unwrap();
 
@@ -1148,10 +1183,6 @@ async fn run() -> Result<(), S2CliError> {
                                         );
                                     }
 
-                                    Ok(ReadOutput::FirstSeqNum(seq_num)) => {
-                                        eprintln!("{}", format!("first_seq_num: {seq_num}").blue().bold());
-                                    }
-
                                     Ok(ReadOutput::NextSeqNum(seq_num)) => {
                                         eprintln!("{}", format!("next_seq_num: {seq_num}").blue().bold());
                                     }
@@ -1185,7 +1216,7 @@ async fn run() -> Result<(), S2CliError> {
             let client_config = client_config(cfg.access_token)?;
             let stream_client = StreamService::new(StreamClient::new(client_config, basin, stream));
 
-            let interval = interval.max(Duration::from_millis(100));
+            let interval = Duration::from(interval).max(Duration::from_millis(100));
             let batch_bytes = batch_bytes.min(128 * 1024);
 
             let prepare_loader = ProgressBar::new_spinner()
@@ -1261,8 +1292,7 @@ async fn run() -> Result<(), S2CliError> {
 
                 let max_jitter = batch_bytes / 4;
 
-                let record_bytes =
-                    jitter_op(batch_bytes, rand::thread_rng().gen_range(0..=max_jitter));
+                let record_bytes = jitter_op(batch_bytes, rand::rng().random_range(0..=max_jitter));
 
                 let Some(res) = pinger.ping(record_bytes).await? else {
                     return Ok(());
@@ -1365,26 +1395,26 @@ async fn run() -> Result<(), S2CliError> {
             auto_prefix_streams,
             basins,
             streams,
-            tokens,
+            access_tokens,
             op_groups,
             ops,
         } => {
             let cfg = config::load_config(&config_path)?;
             let client_config = client_config(cfg.access_token)?;
             let account_service = AccountService::new(Client::new(client_config));
-            let token = account_service
+            let access_token = account_service
                 .issue_access_token(
                     id,
                     expires_at,
                     auto_prefix_streams,
                     basins.map(Into::into),
                     streams.map(Into::into),
-                    tokens.map(Into::into),
+                    access_tokens.map(Into::into),
                     op_groups.map(Into::into),
                     ops.into_iter().map(Into::into).collect(),
                 )
                 .await?;
-            println!("{token}");
+            println!("{access_token}");
         }
         Commands::RevokeAccessToken { id } => {
             let cfg = config::load_config(&config_path)?;

src/stream.rs
@@ -4,8 +4,8 @@ use s2::{
     batching::{AppendRecordsBatchingOpts, AppendRecordsBatchingStream},
     client::StreamClient,
     types::{
-        AppendInput, AppendOutput, AppendRecordBatch, Command, CommandRecord, FencingToken,
-        ReadLimit, ReadOutput, ReadSessionRequest,
+        AppendAck, AppendInput, AppendRecordBatch, Command, CommandRecord, FencingToken, ReadLimit,
+        ReadOutput, ReadSessionRequest, ReadStart, StreamPosition,
     },
 };
 
@@ -64,7 +64,7 @@ impl StreamService {
         Self { client }
     }
 
-    pub async fn check_tail(&self) -> Result<u64, ServiceError> {
+    pub async fn check_tail(&self) -> Result<StreamPosition, ServiceError> {
         self.client
             .check_tail()
             .await
@@ -76,7 +76,7 @@ impl StreamService {
         cmd: CommandRecord,
         fencing_token: Option<FencingToken>,
         match_seq_num: Option<u64>,
-    ) -> Result<AppendOutput, ServiceError> {
+    ) -> Result<AppendAck, ServiceError> {
         let context = match &cmd.command {
             Command::Fence { .. } => ServiceErrorContext::Fence,
             Command::Trim { .. } => ServiceErrorContext::Trim,
@@ -97,7 +97,7 @@ impl StreamService {
         &self,
         stream: impl 'static + Send + Stream<Item = AppendRecord> + Unpin,
         opts: AppendRecordsBatchingOpts,
-    ) -> Result<Streaming<AppendOutput>, ServiceError> {
+    ) -> Result<Streaming<AppendAck>, ServiceError> {
         let append_record_stream = AppendRecordsBatchingStream::new(stream, opts);
 
         self.client
@@ -108,12 +108,12 @@ impl StreamService {
 
     pub async fn read_session(
         &self,
-        start_seq_num: u64,
+        start: ReadStart,
         limit_count: Option<u64>,
         limit_bytes: Option<u64>,
     ) -> Result<Streaming<ReadOutput>, ServiceError> {
         let read_session_req = ReadSessionRequest {
-            start_seq_num,
+            start,
             limit: ReadLimit {
                 count: limit_count,
                 bytes: limit_bytes,

src/types.rs
@@ -508,7 +508,7 @@ impl From<s2::types::AccessTokenInfo> for AccessTokenInfo {
 pub struct AccessTokenScope {
     pub basins: Option<ResourceSet<8, 48>>,
     pub streams: Option<ResourceSet<1, 512>>,
-    pub tokens: Option<ResourceSet<1, 50>>,
+    pub access_tokens: Option<ResourceSet<1, 96>>,
     pub op_groups: Option<PermittedOperationGroups>,
     pub ops: Vec<Operation>,
 }
@@ -518,7 +518,7 @@ impl From<s2::types::AccessTokenScope> for AccessTokenScope {
         AccessTokenScope {
             basins: scope.basins.map(Into::into),
             streams: scope.streams.map(Into::into),
-            tokens: scope.tokens.map(Into::into),
+            access_tokens: scope.access_tokens.map(Into::into),
             op_groups: scope.op_groups.map(Into::into),
             ops: scope.ops.into_iter().map(Operation::from).collect(),
         }

commit e6fc5545c5b1d6ad896f75da9485af0ba82452c1

fix: ping (#138)
broken by https://github.com/s2-streamstore/s2-cli/pull/137

src/ping.rs
@@ -53,7 +53,7 @@ impl Pinger {
             .expect("stream channel open");
 
         match append_stream.next().await.expect("warmup batch ack") {
-            Ok(AppendAck { start, .. }) if start == tail => (),
+            Ok(AppendAck { start, .. }) if start.seq_num == tail.seq_num => (),
             Ok(_) => return Err(S2CliError::PingStreamMutated),
             Err(e) => return Err(ServiceError::new(ServiceErrorContext::AppendSession, e).into()),
         };