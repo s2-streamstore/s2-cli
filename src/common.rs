ad6dc83d6ee2544ab87f5e79dcdc37063d15f145
shikhar <shikhar@s2.dev>
2026-01-19 15:39:26 -0500

    u

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,6 @@
-pub fn epoch_millis() -> u128 {
+pub fn epoch_millis() -> u64 {
     std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
-        .as_millis()
+        .as_millis() as u64
 }
